5e852f0c96fc4dc738e57a1fcb50ba571f302b68
Zenith <mmathfam07@gmail.com>
2026-08-04 19:06:49 +0000

    working on tapestry pages

--- tapestry.js
+++ tapestry.js
@@ -37,3 +37,89 @@ getOpen(cv);
 getOpen(sod);
 getOpen(extra);
 getOpen(essa);
+
+var stories = [
+    {
+        series:"overview",
+        overview: `<p class="disclaimer">!!These pages will contain lore/spoilers about the characters and worlds. 
+          If you would like to bug me to write faster you can reach me <a id="email" href="mailto:starfoxbutbusiness+bookcomplaints@gmail.com">here</a>!!</p>
+          <h3> Tapestry Overview </h3>
+          <p>The Main Tapestry storyline consists of the destruction and reawakening of magic.</p>`,
+        characters: ``,
+        worldbuilding: {
+            species: ``,
+            world: ``,
+            religion: ``,
+            timeline: ``,
+           }  
+        
+    },
+    {
+        series:"Winterr",
+        overview: ``,
+        characters: ``,
+        worldbuilding: {
+            species: ``,
+            world: ``,
+            religion: ``,
+            timeline: ``,
+           }  
+    },
+    {
+        series:"tot",
+        overview: ``,
+        characters: ``,
+        worldbuilding: {
+            species: ``,
+            world: ``,
+            religion: ``,
+            timeline: ``,
+           }  
+    },
+    {
+        series:"cv",
+        overview: ``,
+        characters: ``,
+        worldbuilding: {
+            species: ``,
+            world: ``,
+            religion: ``,
+            timeline: ``,
+           }  
+    },
+    {
+        series:"sod",
+        overview: ``,
+        characters: ``,
+        worldbuilding: {
+            species: ``,
+            world: ``,
+            religion: ``,
+            timeline: ``,
+           }  
+    },
+    {
+        series:"extra",
+        overview: ``,
+        characters: ``,
+        worldbuilding: {
+            species: ``,
+            world: ``,
+            religion: ``,
+            timeline: ``,
+           }  
+    },
+    {
+        series:"essa",
+        overview: ``,
+        characters: ``,
+        worldbuilding: {
+            species: ``,
+            world: ``,
+            religion: ``,
+            timeline: ``,
+           }  
+    },
+
+
+];
