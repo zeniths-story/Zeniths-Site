f53ba2003c4f2e5cf3578e033048a2d7b96599fc
Zenith <mmathfam07@gmail.com>
2026-08-07 15:01:38 +0000

    tapestry

--- tapestry.js
+++ tapestry.js
@@ -1,42 +1,13 @@
-/*var winterr = document.getElementById("winterr");
+var overview = document.getElementById("overview");
+var winterr = document.getElementById("winterr");
 var tot = document.getElementById("tot");
 var CV = document.getElementById("CV");
 var SoD = document.getElementById("SoD");
 var extra = document.getElementById("extra");
 var essa = document.getElementById("essa");
-var overview = document.getElementById("overview");*/
+
 
 var currTab = overview
-
-function openTab(element) {
-    var open = document.getElementById(element.id + "Tab");
-    element.style.display = "flex";
-    open.classList.add("select");
-    currTab = element
-}
-
-function closeTab (element) {
-    var close = document.getElementById(element.id + "Tab");
-    element.style.display = "none";
-    close.classList.remove("select");
-   
-}
-
-function getOpen(element){
-    var tab = document.getElementById(element.id + "Tab");
-    tab.addEventListener("click", function() { 
-        closeTab(currTab);
-        openTab(element);
-    });
-};
-
-/*getOpen(winterr);
-getOpen(overview);
-getOpen(tot)
-getOpen(CV);
-getOpen(SoD);
-getOpen(extra);
-getOpen(essa);*/
 
 var stories = [
     {
@@ -45,17 +16,17 @@ var stories = [
           If you would like to bug me to write faster you can reach me <a id="email" href="mailto:starfoxbutbusiness+bookcomplaints@gmail.com">here</a>!!</p>
           <h3> Tapestry Overview </h3>
           <p>The Main Tapestry storyline consists of the destruction and reawakening of magic.</p>`,
-        characters: ``,
+        characters: `nteb`,
         worldbuilding: {
-            species: ``,
-            world: ``,
-            religion: ``,
-            timeline: ``,
+            species: `breweqzxerdutefvitvyeivteivetitvv7trirbrbrtsg`,
+            world: `brebrdhrftcvribunoyut4drcuiytvrbtyerciyvegrycthrcvyfgdsarhytucxytgdftsfhtgxyufhyu6xzegdsrghjtikugytuefsefhgytdrreggs`,
+            religion: `breaxetvytbutrcstybetvyybrybudtvertvdhvtcvfvrffgfhge`,
+            timeline: `brgeswqxrvyni,o,imk,yhdejghbbjkhlugdrhtjuikldsffegythuiklodsfgrhtyuiklobsergsn6`,
            }  
         
     },
     {
-        series:"Winterr",
+        series:"winterr",
         overview: ``,
         characters: ``,
         worldbuilding: {
@@ -77,7 +48,7 @@ var stories = [
            }  
     },
     {
-        series:"cv",
+        series:"CV",
         overview: ``,
         characters: ``,
         worldbuilding: {
@@ -88,7 +59,7 @@ var stories = [
            }  
     },
     {
-        series:"sod",
+        series:"SoD",
         overview: ``,
         characters: ``,
         worldbuilding: {
@@ -124,25 +95,54 @@ var stories = [
 
 ];
 
-function putTab(element) {
-    for(let i=0; i<stories.length; i++){
-        if (currTab = stories[i].series){
-        //set page content
-        var page = document.getElementById(element.id + "pg");
-        //page.innerHTML = stories[i];
-    }}
-};
-
-function initTab(elementName) {
-    var tab = document.getElementById(elementName.id);
-    getOpen(tab);
-    putTab(tab);
+function openTab(element) {
+    var open = document.getElementById(element.id + "Tab");
+    element.style.display = "flex";
+    open.classList.add("select");
+    currTab = element;
 }
 
-initTab(winterr);
-initTab(overview);
-initTab(tot)
-initTab(CV);
-initTab(SoD);
-initTab(extra);
-initTab(essa);
+function closeTab(element) {
+    var close = document.getElementById(element.id + "Tab");
+    close.classList.remove("select");
+    element.style.display = "none";
+    
+   
+}
+
+function getOpen(element){
+    var tab = document.getElementById(element.id + "Tab");
+    tab.addEventListener("click", function() { 
+        closeTab(currTab);
+        openTab(element);
+        putTab(currTab);
+    });
+};
+
+getOpen(overview);
+getOpen(winterr);
+getOpen(tot)
+getOpen(CV);
+getOpen(SoD);
+getOpen(extra);
+getOpen(essa);
+
+function putTab(element) {
+    for(let i=0; i<stories.length; i++){
+        if (currTab.id == stories[i].series){
+        //set page content
+        var page = document.getElementById(element.id + "pg");
+        page.innerHTML = `
+        ${stories[i].overview}
+        <p>${stories[i].characters}<p/>
+       <details> <summary>Worldbuilding</summary>
+        ${stories[i].worldbuilding.timeline}<br/>
+        ${stories[i].worldbuilding.world}<br/>
+        ${stories[i].worldbuilding.species}<br/>
+        ${stories[i].worldbuilding.religion}
+        
+        </details>
+
+        `;
+    }}
+};
